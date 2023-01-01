4270c8e2827511c99ddca35d6ed684028b95457a
tieu hoa <tieutrunghoa1198@gmail.com>
2023-01-02 02:54:57 +0700

    [UPDATE] update clear queue & code for reading command.ts files

---
+++ src/commands/music/clear.command.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import {players} from "../../models/player";
+import messages from "../../constants/messages";
+import {Command} from "../../constants/command";
+
+export default {
+    data: new SlashCommandBuilder()
+        .setName(Command.clear.name)
+        .setDescription(Command.clear.description),
+    async execute(interaction: any) {
+        await interaction.deferReply();
+        let player = players.get(interaction.guildId as string);
+        if (!player) {
+            await interaction.followUp(messages.playerNotCreated)
+        } else {
+            player.queue = [];
+            await interaction.followUp(messages.emptyQueue);
+        }
+    }
+}
