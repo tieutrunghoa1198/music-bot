c31adea7a6257368ffd16b9d4fce513ad8e748f0
tieu hoa <tieutrunghoa1198@gmail.com>
2025-06-04 14:12:25 +0700

    add log for uncaught exception

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import {logger} from "./core/utils/logger.util";
+
 require('module-alias/register');
 import { Bot } from './bot-application';
 
 Bot.getInstance().start();
 process.on('uncaughtException', function (err) {
-  console.error(err);
-  console.log('Node NOT Exiting...');
+  logger.error(err);
+  logger.info('Node NOT Exiting...');
   return;
 });
